0cf357e08069a51d6e28dc7ad49776100aeff3c7
Hoony <68694844+ghkdwlgns612@users.noreply.github.com>
2021-06-01 14:29:26 +0900

    0601

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -4,6 +4,13 @@
 #include <unistd.h>
 
 
+typedef struct inform {
+    int num;
+    char alpha;
+    char *str;
+    double dot;
+}   inform_list;
+
 char	*ft_strdup(const char *str)
 {
 	size_t	len;
@@ -30,56 +37,53 @@ char	*ft_strdup(const char *str)
 char     *ft_buffer(char *buf, va_list ap)
 {
     char ret;
-
+    int len;
+    
+    len = 0;
     while (1)
     {
         if (*buf == 'c' ||*buf == 's' ||*buf == 'p' ||*buf == 'd' ||*buf == 'i' ||*buf == 'u' ||*buf == 'x' ||*buf == 'X')
             break;
         else
         {
-            ft_flag(*buf,)
         }
         buf++;
     }
-    ft_result()
     ret = *buf;
     buf++;
-
-    
-    if (ret == 'c')
-        return ("char");
-    else if (ret == 's')
-    {}   
-    else if (ret == 'p')
-    {}
-    else if (ret == 'd')
-        return ("int");
-    else if (ret == 'i')
-    {}
-    else if (ret == 'u')
-    {}
-    else if (ret == 'x')
-    {}
-    else if (ret == 'X')
-    {}
 }
 
 
 int     ft_printf(const char *str, ...)
 {
     va_list ap;
-    char *type;
+    char type[3] = {'0','0','0'};
+    char *str1;
     int i = 0;
+    int arg;
     char *buf = ft_strdup(str);
+    inform_list abc;
     
     va_start(ap,str);
     while (*buf)
     {
         if (*buf == '%' && *(buf + 1) != '\0')
         {
-            type = ft_buffer(buf, ap);
+//            str1 = ft_buffer(buf, ap);
             while (*buf && *buf != 'c'&& *buf != 's'&& *buf != 'p'&& *buf != 'd'&& *buf != 'i'&& *buf != 'u'&& *buf != 'x'&& *buf != 'X')
+            {
+                if (*buf == '0')
+                    type[0] = '1';
+                else if(*buf == '-')
+                    type[1] = '1';
+                else if(*buf == '*')
+                    type[2] = '1';
                 buf++;
+            }
+            if (*buf == 'd')
+                abc.num = va_arg(ap, int);
+            else if (*buf == 's')
+                abc.str = va_arg(ap, char *);
             if (*(buf + 1) != '\0')
                 buf++;
         }
@@ -98,7 +102,7 @@ int     ft_printf(const char *str, ...)
 
 int main()
 {
-    ft_printf("[  %0+-d123156%s]",123,"asd");
+   ft_printf("[  %0*s123156]","123133");
+//    printf("%*s",-5 ,"123");
     return 0;
 }
-
