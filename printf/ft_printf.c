af46ba29c242f68be59786d93e4c4e8957cc5c42
Hoony <68694844+ghkdwlgns612@users.noreply.github.com>
2021-06-01 15:37:48 +0900

    Update ft_printf.c

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -11,6 +11,16 @@ typedef struct inform {
     double dot;
 }   inform_list;
 
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
 char	*ft_strdup(const char *str)
 {
 	size_t	len;
@@ -31,6 +41,34 @@ char	*ft_strdup(const char *str)
 	}
 	ret[i] = '\0';
 	return (ret);
+}
+
+
+void    ft_print111(char *res)
+{
+}
+
+
+char    *ft_write(char *res, char *type, void *data)
+{
+    if (type[0] == '1' && type[1] == '1' && type[2] == '1')
+        ft_print111(res);
+    else if (type[0] == '1' && type[1] == '1' && type[2] == '0')
+        ft_print110(res);
+    else if (type[0] == '1' && type[1] == '0' && type[2] == '0')
+        ft_print100(res);
+    else if (type[0] == '1' && type[1] == '0' && type[2] == '1')
+        ft_print101(res);
+    else if (type[0] == '0' && type[1] == '1' && type[2] == '1')
+        ft_print011(res);
+    else if (type[0] == '0' && type[1] == '0' && type[2] == '1')
+        ft_print001(res);
+    else if (type[0] == '0' && type[1] == '1' && type[2] == '0')
+        ft_print010(res);
+    else if (type[0] == '0' && type[1] == '0' && type[2] == '0')
+        ft_print000(res);
+    printf("%s\n",res);
+    return (res);
 }
 
 
@@ -58,9 +96,10 @@ int     ft_printf(const char *str, ...)
 {
     va_list ap;
     char type[3] = {'0','0','0'};
-    char *str1;
+    char *res;
     int i = 0;
-    int arg;
+    int len = 0;
+    int temp = 0;
     char *buf = ft_strdup(str);
     inform_list abc;
     
@@ -69,7 +108,7 @@ int     ft_printf(const char *str, ...)
     {
         if (*buf == '%' && *(buf + 1) != '\0')
         {
-//            str1 = ft_buffer(buf, ap);
+            str1 = ft_buffer(buf, ap);
             while (*buf && *buf != 'c'&& *buf != 's'&& *buf != 'p'&& *buf != 'd'&& *buf != 'i'&& *buf != 'u'&& *buf != 'x'&& *buf != 'X')
             {
                 if (*buf == '0')
@@ -80,10 +119,26 @@ int     ft_printf(const char *str, ...)
                     type[2] = '1';
                 buf++;
             }
-            if (*buf == 'd')
+            if (*buf == 'd') //int형
+            {
                 abc.num = va_arg(ap, int);
-            else if (*buf == 's')
+                temp = abc.num;
+                while (temp)
+                {
+                    temp /= 10;
+                    len++;
+                }
+                res = (char *)malloc(sizeof(char) *(len + 1));
+                res = ft_write(res,type);
+            }
+            else if (*buf == 's') // str형
+            {
                 abc.str = va_arg(ap, char *);
+                len = ft_strlen(abc.str);
+                res = (char *)malloc(sizeof(char) *(len + 1));
+                res = ft_write(res,type);
+            }
+            printf("res : %s\n",res);
             if (*(buf + 1) != '\0')
                 buf++;
         }
@@ -102,7 +157,7 @@ int     ft_printf(const char *str, ...)
 
 int main()
 {
-   ft_printf("[  %0*s123156]","123133");
+   ft_printf("[  %0*s123156]", "1234");
 //    printf("%*s",-5 ,"123");
     return 0;
 }
