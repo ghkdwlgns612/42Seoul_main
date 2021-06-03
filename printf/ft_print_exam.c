53e832bcd378cf5b18804e594a7a50abc5f223b6
Hoony <68694844+ghkdwlgns612@users.noreply.github.com>
2021-06-03 14:05:11 +0900

    0603

--- printf/ft_print_exam.c
+++ printf/ft_print_exam.c
@@ -3,13 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct inform {
+typedef struct data{
     int num;
     char alpha;
     char *str;
-    double dot;
-}   inform_list;
+}   type_variable;
 
+typedef struct inform {
+    int type;
+    int length;
+    int flag;
+    int width;
+    int num;
+    char alpha;
+    char *str;
+}   inform_list;
 
 int	ft_strlen(const char *str)
 {
@@ -54,75 +62,166 @@ char    *ft_forward(char *str)
     return (buf);
 }
 
-int     ft_zero_minus(char *str)
+
+int     ft_int_length(int num)
 {
-    if ()
-        if ((*str == '0' && *(str + 1) == '-') && (*str == '-' && *(str + 1) == '0'))
+    int cnt = 0;
+    while (num)
+    {
+        num /= 10;
+        cnt++;
+    }
+    return (cnt);
+}
+
+void    ft_init(inform_list *inform)
+{
+    inform->type = 0;
+    inform->length = 0;
+    inform->flag = 0;
+    inform->width = 0;
+    inform->num = 0;
+    inform->alpha = 0;
+    inform->str = 0;
+}
+
+void    ft_type(inform_list *inform, char *buf)
+{
+    int i = 0;
+
+
+    while (buf[i] && (buf[i] != 'c' || buf[i] != 's' || buf[i] != 'd'))
+        i++;
+    if (buf[i - 1] == 'd')
+        inform->type = 1;
+    else if (buf[i - 1] == 'c')
+        inform->type = 2;
+    else if (buf[i -1] == 's')
+        inform->type = 3;
+}
+
+void    ft_length(inform_list *inform, char *buf, va_list ap)
+{
+    switch (inform->type)
+    {
+        case 1 :
+            inform->num = va_arg(ap,int);
+            if (inform->num < 0)
+                inform->length = ft_int_length(-inform->num) + 1;
+            else
+                inform->length = ft_int_length(inform->num);
+            break;
+        case 2 :
+            inform->str = va_arg(ap,char *);
+            inform->length = 1;
+            break;
+        case 3 :
+            inform->str = va_arg(ap,char *);
+            inform->length = ft_strlen(inform->str);
+            break;
+        default:
+            break;
+    }
+}
+
+void    ft_flag(inform_list *inform, char *buf)
+{
+    int i = 0;
+    while(buf[i] && buf[i] != '%')
+        i++;
+    i++;
+    if (buf[i] == '0')
+        inform->flag = 1; //0 채우기
+    else if (buf[i] == '-' || (buf[i] == '-' && buf[i + 1] == '0') || (buf[i] == '0' && buf[i + 1] == '-'))
+        inform->flag = 2; //- 채우기
+    else
+        inform->flag = 3; //아무 플래그 없음
+}
+
+void    ft_width(inform_list *inform, char *buf, va_list ap)
+{
+    int i = 0;
+    while (buf[i])
+    {
+        if (buf[i] == '*')
         {
-            ft_start_left();
+            inform->width = va_arg(ap,int);
+            break ;
         }
-        else if ()
-    return (1);
+        else if (buf[i] >= '1' && buf[i] <= '9')
+        {
+            while (buf[i] >= '0' && buf[i] <= '9')
+            {
+                inform->width *= 10;
+                inform->width += buf[i++] - '0';
+            }
+            break ;
+        }
+        else
+        {
+        //    출력
+        }
+        
+        i++;
+    }
+}
+
+void    ft_int_print(inform_list *inform, char *str)
+{
+    int temp = inform->num;
+    int len = inform->length;
+    str[len--] = '\0';
+    while (temp)
+    {
+        str[len--] = (temp % 10) + '0';
+        temp /= 10;
+    }
+    while (*str)
+    {
+        write(1,str,1);
+        str++;
+    }
 }
 
 int     ft_printf(const char *str, ...)
 {
     va_list ap;
-    inform_list inform;
-    int result_cnt = 0;
-    char *buf;
-    int len;
-    
-    buf = ft_strdup(str);
+    inform_list *my_inform;
+    char *buf = ft_strdup(str);
+    char *res;
     va_start(ap,str);
+
+    // printf("%d\n",my_inform->type); // 1은 int형, 2는 char형, 3은 char *형
+    // printf("%d\n",my_inform->length); // 문자 및 숫자의 길이
+    // printf("%d\n",my_inform->width); // 중간 인자에 대한 것
+    // printf("%d\n",my_inform->flag); // 1은 0채우기, 2는 왼쪽정렬, 3은 아무 플래그없음.  
     while (*buf)
     {
-        if (*buf == '%')
+        ft_init(my_inform);
+        ft_type(my_inform, buf);
+        ft_width(my_inform, buf,ap);
+        ft_length(my_inform,buf,ap);
+        ft_flag(my_inform, buf); break ;
+
+        if (my_inform->type == 1)
         {
-            buf++;
-            arg = va_arg(ap,ft_select_type(*(buf - 1)));
-            while (*buf != '%' || *buf != '\0')
+            if (my_inform->width == 0)
             {
-                if (*buf == '0' || *buf == '-' || (*buf == '0' && *(buf + 1) == '-') || (*buf == '-' && *(buf + 1) == '0')) 
-                {
-                    result_cnt = ft_zero_minus(buf);
-                    buf = ft_forward(buf);
-                }
-                else if (*buf == '*')
-                {
-//                    result_cnt = ft_star_width(buf);
-                    buf = ft_forward(buf);
-                }
-                else if (*buf == '.')
-                {
- //                   result_cnt = ft_precision(buf);
-                    buf = ft_forward(buf);
-                }
-                else if (*buf >= '1' && *buf <= '9')
-                {
- //                   result_cnt = ft_width();
-                    buf = ft_forward(buf);
-                }
-                else if (*buf == 'a')
-                {
- //                   result_cnt = ft_print_case();
-                    buf = ft_forward(buf);
-                }
-                else
-                    break ;
+                res = (char *)malloc(sizeof(char) * (my_inform->length + 1));
+                ft_int_print(my_inform, res);
             }
+            buf = ft_forward(buf);
         }
-        else
-        {
-            write(1,buf++,1);
-        }
+        buf++;
+        free(res);
     }
-    return (result_cnt);
+    printf("1");
+    return (1);
 }
 
-int     main()
+int main()
 {
-    ft_printf("  %0 312312","123");
-//    printf("%05  d",1234);
+    ft_printf("%0d",-1234566);
+//    printf("%05d",123);
     return 0;
 }
