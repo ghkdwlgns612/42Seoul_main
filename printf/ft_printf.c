303db339788d2fb3b40bb4e790f5660390e03fee
Hoony <ghkdwlgns612@naver.com>
2021-06-07 13:15:33 +0900

    commit check

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -7,7 +7,7 @@ int    ft_res_strlen(inform_list *inform, char *str)
     else
         return (ft_strlen(str) + 1);
 }
-
+hello
 int    ft_int_print(inform_list *inform, char *str)
 {
     int temp = inform->num;
@@ -41,7 +41,7 @@ int     ft_printf(const char *str, ...)
     char *res;
     int len;
     va_start(ap,str);
-    
+    hello;
     while (*buf)
     {
         my_inform = (inform_list *)malloc(sizeof(inform_list));
