82b3494dd6ac6be4471e218524fd53bff25f8a0c
Hoony <68694844+ghkdwlgns612@users.noreply.github.com>
2021-06-09 10:09:51 +0900

    0609

--- printf_prac/main.c
+++ printf_prac/main.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct inform {
+    int precision;
+    int width;
+}   inform_list;
+
+void    ft_init(inform_list *inform)
+{
+    int precision;
+    int width;
+}
+
 int     ft_star(va_list ap)
 {
     return (va_arg(ap,int));
@@ -30,12 +41,19 @@ char	*ft_strdup(const char *str)
 	return (ret);
 }
 
+// int    ft_cal(char *str, va_list ap)
+// {
+//     int num;
+//     num = 0;
+    
+//     return (num);
+// }
 
 int     ft_chk_precision(char *str, va_list ap)
 {
     int i;
     int num;
-
+    
     num = 0;
     i = 0;
     while (str[i] != '\0' && str[i] != '%')
@@ -61,23 +79,52 @@ int     ft_chk_precision(char *str, va_list ap)
     return (num);
 }
 
+int     ft_width(char *str, va_list ap)
+{
+    int i;
+    int num;
+
+    i = 0;
+    while (str[i] != '\0' && str[i] != '%' && str[i] != '.')
+    {
+        if (str[i] == '*')
+            num = ft_star(ap);
+        else if (str[i] > '0' && str[i] < '9')
+        {
+            while (str[i] > '0' && str[i] < '9')
+            {
+                num *= 10;
+                num += str[i++] - '0';
+            }
+        }
+        else
+            num = 0;
+        i++;
+    }
+    return(num);
+}
+
 int     ft_printf(const char *str, ...)
 {
     va_list ap;
+    inform_list *inform;
     char *buf;
     int i;
-    int precision_num;
-
+    
     buf = ft_strdup(str);
     va_start(ap,str);
     i = 0;
     while (buf[i])
     {
+        inform = (inform_list *)(malloc)(sizeof(inform_list));
         while (buf[i] != '%' && buf[i] != '\0')
             write(1,&buf[i++],1);
         i++;
-        precision_num = ft_chk_precision(&buf[i], ap);
-        printf("%d\n",precision_num);
+        inform->width = ft_width(&buf[i], ap);
+        inform->precision = ft_chk_precision(&buf[i], ap);
+        printf("%d\n",inform->width);
+        printf("%d\n",inform->precision);
+        free(inform);
         break ;
     }
     free(buf);
@@ -86,5 +133,5 @@ int     ft_printf(const char *str, ...)
 
 int main()
 {
-    ft_printf("%.*d",29);
+    ft_printf("%*.*d",29,30);
 }
