5796b1103028f094251ff2176ea3bf048e0debd9
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-09-06 16:46:22 +0900

    유효성 검증 및 범위설정

--- push_swap/push.h
+++ push_swap/push.h
@@ -1,5 +1,28 @@
 #ifndef PUSH_H
 #define PUSH_H
 #include <stdio.h>
+#include <stdlib.h>
+
+#define INT_MAX = 2147483647;
+#define INT_MIN = -2147483648;
+
+typedef struct		s_node
+{
+    int     value;
+    struct s_node  *prev;
+    struct s_node  *next;
+}					t_node;
+
+typedef struct s_stack
+{
+    int     size;
+    t_node  *top;
+    t_node  *bottom;
+}              t_stack;
+
+
+long long	    ft_atoi(const char *str);
+size_t	ft_strlen(const char *str);
+int    ft_check_arg(const char *str);
 
 #endif
--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -1,5 +1,84 @@
 #include "../push.h"
 
-int main() {
+int				ft_minus(char c)
+{
+	if (c == '+' || c == '-')
+	{
+		if (c == '-')
+			return (-1);
+	}
+	return (1);
+}
+
+long long				ft_atoi(const char *str)
+{
+	int			i;
+	int			minus;
+	long long	res;
+
+	i = 0;
+	minus = 1;
+	res = 0;
+	while (str[i] && (str[i] == ' ' || (str[i] <= 13 && str[i] >= 9)))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			minus = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res *= 10;
+		res += str[i] - 48;
+		i++;
+	}
+	return (minus * res);
+}
+
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+    return (i);
+}
+
+
+int ft_check_arg(const char *str) { //str -> 부호 제외하고 10자리인지 확인 -> long long변환 후 int_max , int_min랑 확인
+    int i = 0;
+    int len = ft_strlen(str);
+    //숫자로 사용될수있는지 확인(1234, -1234, 12-34, 12a2, -12-12)
+    while (str[i] != 0)
+    {
+        if (str[i] < '0' || str[i] > '9')
+        {
+            if (!(i == 0 && str[i] == '-'))
+            {
+                printf("Error\n");
+                exit(0);
+            }
+        }
+        i++;
+    }
+    //정수범위내에 들어오는지 확인
+    long long ret = ft_atoi(str);
+    if (ret > 2147483647 || ret < -2147483648)
+    {
+        printf("Error\n");
+        exit(0);
+    }
+    return (int)ret;
+}
+
+
+int main(int argc, char *argv[]) {
+    int i = 1;
+    int arr[argc - 1];
+    while(i < argc)
+        arr[i-1] = ft_check_arg(argv[i++]);
     return 0;
 }
